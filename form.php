fcdf21b3c5930798d8574727ce496b6db6dcc2d6
ZinaSzt <iukwim404@gmail.com>
2023-03-25 23:46:14 +0100

    ajout du formulaire

--- form.php
+++ form.php
@@ -1,33 +1,88 @@
-<form action="/thanks.php" method="post">
-    
-    <div>
-        <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="last_name">
-    </div>
-    <div>
-        <label for="prenom">Prenom :</label>
-        <input type="text" id="prenom" name="first_name">
-    </div>
-    <div>
-        <label for="phone">Phone Number :</label>
-        <input type="text" id="phone" name="phone_number">
-    </div>
-    <div>
-        <label for="courriel">Email :</label>
-        <input type="email" id="courriel" name="user_email">
-    </div>
-    <label for="subject">Subject:</label>
-    <select id="subject" name="subject">
-        <option value="Valorant">Valorant</option>
-        <option value="Days Gone">Days Gone</option>
-        <option value="Dishonored">Dishonored</option>
-        <option value="Elden Ring">Elden Ring</option>
-    </select>
-    <div>
-        <label for="message">Message :</label>
-        <textarea id="message" name="user_message"></textarea>
-    </div>
-    <div class="button">
-        <button type="submit">Send</button>
-    </div>
-</form>
+<?php
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!isset($_POST['last_name']) || trim($_POST['last_name']) === '') {
+        $errors[] = 'Veuillez saisir votre nom';
+    }
+
+    if (!isset($_POST['first_name']) || trim($_POST['first_name']) === '') {
+        $errors[] = 'Veuillez saisir votre prénom';
+    }
+
+    if (!isset($_POST['user_email']) || trim($_POST['user_email']) === '') {
+        $errors[] = 'Veuillez saisir votre adresse email';
+    } else {
+        $email = $_POST['user_email'];
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors[] = 'Veuillez saisir une adresse email valide';
+        }
+    }
+
+    if (!isset($_POST['phone_number']) || trim($_POST['phone_number']) === '') {
+        $errors[] = 'Veuillez saisir votre numéro de téléphone';
+    }
+
+    if (!isset($_POST['subject']) || trim($_POST['subject']) === '') {
+        $errors[] = 'Veuillez choisir un sujet';
+    }
+
+    if (!isset($_POST['user_message']) || trim($_POST['user_message']) === '') {
+        $errors[] = 'Veuillez saisir votre message';
+    }
+
+    if (empty($errors)) {
+        header('Location: thanks.php');
+        exit;
+    } else {
+        $errors[] = 'Veuillez corriger les erreurs suivantes :';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulaire de contact</title>
+    <link rel="stylesheet" href="./form.css">
+</head>
+<body>
+
+<form action="thanks.php" method="post">
+    <div class= "center">
+        <h1> Contact Us</h1>
+        <div class= "nom">
+            <input type="text" id="nom" name="last_name" required>
+            <label for="nom">Nom </label>
+        </div>
+        <div class= "prenom">
+            <input type="text" id="prenom" name="first_name" required>
+            <label for="prenom">Prenom</label>
+        </div>
+        <div class = "phone">
+            <input type="text" id="phone" name="phone_number" required>
+            <label for="phone">Phone Number</label>
+        </div>
+        <div class= "courriel">
+            <input type="email" id="courriel" name="user_email" required>
+            <label for="courriel">Email :</label>
+        </div>
+        <div class = "select">
+            <label for="subject">Subject:</label><br>
+            <select id="subject" name="subject" required>
+                <option value="" disabled selected>Choisissez un sujet</option>
+                <option value="Valorant">Valorant</option>
+                <option value="Days Gone">Days Gone</option>
+                <option value="Dishonored">Dishonored</option>
+                <option value="Elden Ring">Elden Ring</option>
+            </select>
+        </div>
+        <div class= "message">
+            <textarea id="message" name="user_message" required></textarea>
+            <label for="message">Message</label>
+        </div>
+        <div class="button">
+            <button type="submit">Send</button>
+        </div>
+   
--- thanks.php
+++ thanks.php
@@ -1,14 +1,22 @@
 <?php
-$nom = $_POST['last_name'];
 $prenom = $_POST['first_name'];
-$phone = $_POST['phone_number'];
-$courriel = $_POST['user_email'];
+$nom = $_POST['last_name'];
 $game = $_POST['subject'];
+$courriel = $_POST['user_email'];
+$phone = $_POST['phone_number'];
 $message = $_POST['user_message'];
-
-echo "Merci $prenom $nom de nous avoir contacté à propos de $game.
-
-Un de nos conseillers vous contactera soit à l’adresse $courriel ou par téléphone au $phone dans les plus brefs délais pour traiter votre demande : 
-
-$message";
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulaire de contact - Merci</title>
+    <link rel="stylesheet" href="./form.css">
+</head>
+<body>
+    Merci <?= $prenom ?> <?= $nom ?> de nous avoir contacté à propos de <?= $game ?>.
+    <p>Un de nos conseillers vous contactera soit à l’adresse <?= $courriel ?> ou par téléphone au <?= $phone ?> dans les plus brefs délais pour traiter votre demande :</p>
+    <p><?= $message ?></p>
+</body>
+</html>
